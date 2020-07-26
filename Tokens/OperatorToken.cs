3ea894e27a8f19a43376e8edeb47918deb8408ac
Blokyk <eliot.courvoisier@gmail.com>
2020-07-26 14:11:19 +0200

    Fixed inconsistency in OperatorToken's constructor
    You couldn't pass leading and trailing TriviaToken to the constructor

--- Tokens/OperatorToken.cs
+++ Tokens/OperatorToken.cs
@@ -6,27 +6,27 @@ public class OperatorToken : Token
     public bool IsLeftAssociative { get; protected set; }
 
 
-    public OperatorToken(string representation, Precedence precedence, bool isLeftAssociative, Location location)
-        : base(representation, TokenKind.@operator, location)
+    public OperatorToken(string representation, Precedence precedence, bool isLeftAssociative, Location location, TriviaToken? leading = null, TriviaToken? trailing = null)
+        : base(representation, TokenKind.@operator, location, leading, trailing)
     {
         Precedence = precedence;
         IsLeftAssociative = isLeftAssociative;
     }
 
-    public OperatorToken(string representation, Precedence precedence, string associativity, Location location)
-        : this(representation, precedence, associativity == "left", location)
+    public OperatorToken(string representation, Precedence precedence, string associativity, Location location, TriviaToken? leading = null, TriviaToken? trailing = null)
+        : this(representation, precedence, associativity == "left", location, leading, trailing)
     { }
 
-    public OperatorToken(char representation, Precedence precedence, string associativity, Location location)
-        : this(representation.ToString(), precedence, associativity == "left", location)
+    public OperatorToken(char representation, Precedence precedence, string associativity, Location location, TriviaToken? leading = null, TriviaToken? trailing = null)
+        : this(representation.ToString(), precedence, associativity == "left", location, leading, trailing)
     { }
 
-    public OperatorToken(char representation, Precedence precedence, bool isLeftAssociative, Location location)
-        : this(representation.ToString(), precedence, isLeftAssociative, location)
+    public OperatorToken(char representation, Precedence precedence, bool isLeftAssociative, Location location, TriviaToken? leading = null, TriviaToken? trailing = null)
+        : this(representation.ToString(), precedence, isLeftAssociative, location, leading, trailing)
     { }
 
-    public OperatorToken(Token token, Precedence precedence, string associativity, Location location)
-        : this(token, precedence, associativity == "left", location)
+    public OperatorToken(Token token, Precedence precedence, string associativity, Location location, TriviaToken? leading = null, TriviaToken? trailing = null)
+        : this(token, precedence, associativity == "left", location, leading, trailing)
     {
         Kind = token.Kind == TokenKind.function ? TokenKind.function : TokenKind.@operator;
     }

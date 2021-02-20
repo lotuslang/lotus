960a24e39e5326d7d85ca654c252e17bb98e5448
Blokyk <eliot.courvoisier@gmail.com>
2021-02-20 13:08:29 +0100

    Make Location a record + new methods to utilities
    Also, added a .editorconfig file and upped the .net version (from
    netcoreapp3.1 to net5)

--- Utilities.cs
+++ Utilities.cs
@@ -47,8 +47,14 @@ public static class Utilities
     public static ReadOnlyCollection<T> AsReadOnly<T>(this ICollection<T> list)
         => list.ToList().AsReadOnly();
 
+    /// <summary>
+    /// Tries to find an element in the collection that matches the condition
+    /// </summary>
+    /// <param name="collection">The collection to search in</param>
+    /// <param name="match">The condition to search for</param>
+    /// <returns>Either the first element that matches ; or default(T)</returns>
     [return: MaybeNull]
-    public static T Find<T>(this ICollection<T> collection, Predicate<T> match) {
+    public static T? Find<T>(this ICollection<T> collection, Predicate<T> match) {
         if (match is null) {
             throw new ArgumentNullException(nameof(match));
         }
@@ -57,7 +63,24 @@ public static class Utilities
             if (match(item)) return item;
         }
 
-        return default(T)!;
+        return default(T);
+    }
+
+    /// <summary>
+    /// Checks whether the collection contains an element that matches the condition
+    /// </summary>
+    /// <param name="collection">The collection to search in</param>
+    /// <param name="match">The condition to check against for</param>
+    public static bool Contains<T>(this ICollection<T> collection, Predicate<T> match) {
+        if (match is null) {
+            throw new ArgumentNullException(nameof(match));
+        }
+
+        foreach (var item in collection) {
+            if (match(item)) return true;
+        }
+
+        return false;
     }
 
     public static string Join<T>(string separator, Func<T, string> convert, params T[] value) => Join(separator, convert, coll: value);
@@ -90,6 +113,40 @@ public static class Utilities
         }
     }
 
+	public static (List<T> valid, List<T> invalid) Split<T>(this IEnumerable<T> list, Predicate<T> match) {
+		var (valid, invalid) = (new List<T>(), new List<T>());
+
+		foreach (var item in list) {
+			if (match(item)) valid.Add(item);
+			else invalid.Add(item);
+		}
+
+		return (valid, invalid);
+	}
+
+	public static (List<TValid> valid, List<TInvalid> invalid) SplitByType<TList, TValid, TInvalid>(this IEnumerable<TList> list)
+		where TInvalid : class, TList
+	{
+		var (valid, invalid) = (new List<TValid>(), new List<TInvalid>());
+
+		foreach (var item in list) {
+			if (item is TValid validItem) valid.Add(validItem);
+			else invalid.Add((item as TInvalid)!);
+		}
+
+		return (valid, invalid);
+	}
+
+	public static (List<TMatch> valid, List<TOther> invalid) SplitByType<TMatch, TOther>(this IEnumerable<TOther> list) where TOther : class
+		=> SplitByType<TOther, TMatch, TOther>(list);
+
+	// the loops you have to jump through sometimes...
+	public static IEnumerable<TMatch> WhereType<T, TMatch>(this IEnumerable<T> list) {
+		foreach (var item in list) {
+			if (item is TMatch matched) yield return matched;
+		}
+	}
+
     public static GraphNode Apply(this GraphNode node, Func<GraphNode, GraphNode> transform) {
         foreach (GraphNode child in node.Children) {
             child.Apply(transform);
@@ -97,4 +154,11 @@ public static class Utilities
 
         return transform(node);
     }
+
+	public static Stack<T> Clone<T>(this Stack<T> original) {
+		var arr = new T[original.Count];
+		original.CopyTo(arr, 0);
+		Array.Reverse(arr);
+		return new Stack<T>(arr);
+	}
 }

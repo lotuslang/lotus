6e96da1a5d7a9e76c30839e5f1022b9bbc5dd3dd
Blokyk <eliot.courvoisier@gmail.com>
2021-06-26 19:19:16 +0200

    Added ASTHelper.PrintNode and fixed ValuePrinter
    ASTHelper.PrintNode can be used to print any node, it's useful if you
    don't know the its type. I also moved the "interpolated string" logic
    from ValuePrinter to TokenPrinter, since they are basically the same.

--- Visitors/StatementVisitor/ValuePrinter.cs
+++ Visitors/StatementVisitor/ValuePrinter.cs
@@ -17,76 +17,9 @@ public sealed class ValuePrinter : StatementVisitor<string>
     public override string Visit(BoolNode node)
         => ASTHelper.PrintToken(node.Token);
 
-    public override string Visit(ComplexStringNode node) {
-        var output = new StringBuilder("\"");
-
-        var str = node.Value;
-
-        // please don't ask, i was "in the flow" (and really hungry too so goodbye)
-        for (var i = 0; i < node.Value.Length; i++) {
-            if (str[i] != '{' && str[i] != '\\') {
-                output.Append(str[i]);
-                continue;
-            }
-
-            if (i + 1 < str.Length && str[i+1] == '{') {
-                output.Append(str[i]);
-                continue;
-            }
-
-            // This code does three things :
-            // 1. Extract the code section this piece of the string refers to
-            // 2. Pretty print that code section
-            // 3. Increment `i` by the right amount so that we don't double-parse or ignore anything
-
-            // Example
-            // Let's say we have the string "hello {name} !"
-            // It will get translated by the tokenizer and parser as
-            // ComplexStringNode("hello {0} !", codeSections = [ IdentNode("name") ])
-            // and we have just lost the information of where `name` was in the string
-            //
-            // To pretty-print it, we have to put back "name" into its place while not ignoring any other character.
-            // To do so, we first add every character before the "{0}" part to `output`.
-            //
-            // Note : the "{0}" part is called a "section marker" or simply a marker.
-            //
-            // 1. Then, when we arrive on '{', we initialize a counter that tracks how many digits this marker contains
-            // In our case, it contains only 1 digit, so we have digitCount = 1. Finally, we extract the index from the
-            // marker, by extracting a string (`sectionIndexStr`) and then parsing it into a number (`sectionIndex`).
-            //
-            // 2. Once we have the index, we can pretty-print the code section (`sectionStr`)
-            //
-            // 3. To clean-up, we have to increment `i` by the number of characters we handled ourselves (instead of through the main loop).
-            // Since we consumed the index (which has digitCount = 1) and we don't want to print the closing '}' at the end of the marker,
-            // we increment `i` by the number of digits + 1, to account for '}'
-
-            var digitCount = 0;
-
-            // increment digitCount while the character is not '}'
-            while (str[digitCount + 1 + i] != '}') digitCount++;
-
-            // extract the digits between '{' and '}'
-            var sectionIndexStr = str.AsSpan(i + 1, digitCount);
-
-            // parse the digit string into the index of code section this refers to.
-            var sectionIndex = Int32.Parse(sectionIndexStr);
-
-            // pretty-print the code section
-            var sectionStr = Print(node.CodeSections[sectionIndex]);
-
-            output.Append("{" + sectionStr + "}");
-
-            i += digitCount + 1; // the `+ 1` is to account for the closing "}" at the end
-        }
-
-        return output.ToString() + '"';
-    }
-
     public override string Visit(FunctionCallNode node)
         => Print(node.FunctionName)
-         + ASTHelper.PrintToken(node.ArgList.OpeningToken)
-         + Utilities.Join(",", Print, node.ArgList)
-         + ASTHelper.PrintToken(node.ArgList.ClosingToken);
+         + Utilities.Join(",", Print, node.ArgList);
 
     public override string Visit(ObjectCreationNode node)
         => ASTHelper.PrintToken(node.Token) + Print(node.InvocationNode);
@@ -143,17 +76,10 @@ public sealed class ValuePrinter : StatementVisitor<string>
         }
     }
 
-    public override string Visit(ParenthesizedValueNode node)
-        => ASTHelper.PrintToken(node.OpeningToken) + Print(node.Values) + ASTHelper.PrintToken(node.ClosingToken);
-
-
-    public override string Visit(TupleNode node) {
-        var output = ASTHelper.PrintToken(node.OpeningToken);
-
-        foreach (var value in node.Values) output += Print(value) + ",";
-
-        return output + ASTHelper.PrintToken(node.ClosingToken);
-    }
+    public override string Visit(TupleNode node)
+        => ASTHelper.PrintToken(node.OpeningToken)
+         + Utilities.Join(",", Print, node.Values)
+         + ASTHelper.PrintToken(node.ClosingToken);
 
     public override string Visit(SimpleBlock block)
         => throw Logger.Fatal(new InvalidCallException("ValuePrinter cannot print blocks", block.Location));
--- Visitors/TokenVisitor/TokenPrinter.cs
+++ Visitors/TokenVisitor/TokenPrinter.cs
@@ -1,12 +1,80 @@
+using System;
+using System.Text;
+
 public sealed class TokenPrinter : TokenVisitor<string>
 {
-    public override string Visit(ComplexStringToken token) => Default(token);
-
     public override string Visit(NumberToken token)
         => PrintLeadingTrivia(token) + token.Representation + PrintTrailingTrivia(token);
 
     public override string Visit(StringToken token)
         => PrintLeadingTrivia(token) + '"' + token.Representation + '"' + PrintTrailingTrivia(token);
+
+    public override string Visit(ComplexStringToken token) {
+        var output = new StringBuilder("\"");
+
+        var str = token.Representation;
+
+        // please don't ask, i was "in the flow" (and really hungry too so goodbye)
+        for (var i = 0; i < token.Representation.Length; i++) {
+            if (str[i] != '{' && str[i] != '\\') {
+                output.Append(str[i]);
+                continue;
+            }
+
+            if (i + 1 < str.Length && str[i + 1] == '{') {
+                output.Append(str[i]);
+                continue;
+            }
+
+            // This code does three things :
+            // 1. Extract the code section this piece of the string refers to
+            // 2. Pretty print that code section
+            // 3. Increment `i` by the right amount so that we don't double-parse or ignore anything
+
+            // Example
+            // Let's say we have the string "hello {name} !"
+            // It will get translated by the tokenizer and parser as
+            // ComplexStringNode("hello {0} !", codeSections = [ IdentNode("name") ])
+            // and we have just lost the information of where `name` was in the string
+            //
+            // To pretty-print it, we have to put back "name" into its place while not ignoring any other character.
+            // To do so, we first add every character before the "{0}" part to `output`.
+            //
+            // Note : the "{0}" part is called a "section marker" or simply a marker.
+            //
+            // 1. Then, when we arrive on '{', we initialize a counter that tracks how many digits this marker contains
+            // In our case, it contains only 1 digit, so we have digitCount = 1. Finally, we extract the index from the
+            // marker, by extracting a string (`sectionIndexStr`) and then parsing it into a number (`sectionIndex`).
+            //
+            // 2. Once we have the index, we can pretty-print the code section (`sectionStr`)
+            //
+            // 3. To clean-up, we have to increment `i` by the number of characters we handled ourselves (instead of through the main loop).
+            // Since we consumed the index (which has digitCount = 1) and we don't want to print the closing '}' at the end of the marker,
+            // we increment `i` by the number of digits + 1, to account for '}'
+
+            var digitCount = 0;
+
+            // increment digitCount while the character is not '}'
+            while (str[digitCount + 1 + i] != '}') digitCount++;
+
+            // extract the digits between '{' and '}'
+            var sectionIndexStr = str.AsSpan(i + 1, digitCount);
+
+            // parse the digit string into the index of code section this refers to.
+            var sectionIndex = Int32.Parse(sectionIndexStr);
+
+            output.Append("{");
+
+            foreach (var sectionToken in token.CodeSections[sectionIndex])
+                output.Append(Print(sectionToken));
+
+            output.Append("}");
+
+            i += digitCount + 1; // the `+ 1` is to account for the closing "}" at the end
+        }
+
+        return output.ToString() + '"';
+    }
 
     public override string Visit(Token token) {
         var output = PrintLeadingTrivia(token);

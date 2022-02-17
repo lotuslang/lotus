93a6518f6e031b43d69e8646533844a8b45d80cd
Blokyk <eliot.courvoisier@gmail.com>
2022-02-17 15:33:34 +0100

    Clean up Location.cs

--- Logging/Location.cs
+++ Logging/Location.cs
@@ -1,17 +1,6 @@
-/*public struct LocationR
+public record Location(int line, int column, string filename = "<std>")
 {
-    public static readonly Location NULL = new Location(-1, -1);
-
-    public int line;
-    public int column;
-
-    public readonly string filename;
-
-    public Location(int line, int column, string filename = "<std>") {
-        this.line = line;
-        this.column = column;
-        this.filename = filename;
-    }
+    public static readonly Location NULL = new(-1, -1);
 
     public override string ToString()
         => this;
@@ -22,17 +11,8 @@
         filename = this.filename;
     }
 
-    public static implicit operator LocationRange(Location loc) => new LocationRange(loc, loc);
-
-    public static implicit operator string(Location loc) {
-        return $"{loc.filename}({loc.line}, {loc.column})";
-    }
-}*/
-
-public record Location(int line, int column, string filename = "<std>") {
-    public static readonly Location NULL = new(-1, -1);
-
-    public static implicit operator LocationRange(Location loc) => new(loc, loc);
+    public static implicit operator LocationRange(Location loc)
+        => new(loc, loc);
 
     public static implicit operator string(Location loc)
         => $"{loc.filename}({loc.line}:{loc.column})";
@@ -45,7 +25,4 @@ public record Location(int line, int column, string filename = "<std>") {
         => loc1.line == loc2.line
                 ? loc1.column > loc2.column
                 : loc1.line > loc2.line;
-
-    public override string ToString()
-        => this;
 }

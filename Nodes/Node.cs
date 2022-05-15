fa973bb92739b8bc8c5b8bfd952d71e401b12eaa
Blokyk <eliot.courvoisier@gmail.com>
2022-05-15 03:52:33 +0200

    Node.IsValid and Token.IsValid are now get-set

--- Nodes/Node.cs
+++ Nodes/Node.cs
@@ -1,5 +1,15 @@
-public abstract record Node(Token Token, LocationRange Location, bool IsValid = true) {
+public abstract record Node {
     public static readonly Node NULL = ValueNode.NULL;
+
+    public Token Token { get; init; }
+    public LocationRange Location { get; init; }
+    public bool IsValid { get; set; } = true;
+
+    public Node(Token token, LocationRange location, bool isValid = true) {
+        Token = token;
+        Location = location;
+        IsValid = isValid;
+    }
 
     protected Node(Token token, bool isValid = true) : this(token, token.Location, isValid) { }
 }

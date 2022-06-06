528b6324c7fcbecfe31155b6ca54e76865499139
Blokyk <eliot.courvoisier@gmail.com>
2022-06-06 21:17:35 +0200

    Return correct exit code and add --force option
    The --force/-f option can be used to ignore compilation errors when
    executing a command (like 'graph' or 'print'). This doesn't mean that
    the exit code will always be 0, but if the AST isn't in a terribly
    corrupt/fucked up state, it's pretty much guaranteed.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ using System.Globalization;
 #pragma warning disable
 class Program
 {
-    static void Main(string[] args) {
+    static int Main(string[] args) {
 #if VS
-		// Lil hack for our visual studio (win and mac) users, whose IDE thinks it's a rebel
+        // Lil hack for our visual studio (win and mac) users, whose IDE thinks it's a rebel
         // because it doesn't use the same working directory as literally every other
         // major IDE + the official fucking CLI. Used to love vs 2019, but honestly
         // I think I'm switching to vs code for most things and not loooking back.
@@ -22,9 +22,15 @@ class Program
 
         CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
+        var argList = args.ToList();
+
+        var forced = argList.RemoveAll(arg => arg is "--force" or "-f") > 0;
+
+        args = argList.ToArray();
+
         if (args.Length == 0 || args[0] == "help") {
             Console.Error.WriteLine(@"
-Usage : dotnet run -- [option]
+Usage:  dotnet run -- [option]
         dotnet run
         parsex [option]
         parsex
@@ -36,9 +42,11 @@ Usage : dotnet run -- [option]
         constant          print the hash of the constant-colored AST
     graph             print dot code of the AST
         constant          print dot code of the AST with constant-coloring
+
+Note: '--force' or '-f' ignores compilation errors before executing above commands
             ");
 
-            return;
+            return 1;
         }
 
         var file = new Uri(Directory.GetCurrentDirectory() + "/./test.txt");
@@ -56,15 +64,17 @@ Usage : dotnet run -- [option]
             var count = Logger.ErrorCount;
             Logger.PrintAllErrors();
             Console.Error.WriteLine("In total, there were " + count + " errors. Please fix them.");
-            return;
+
+            if (!forced)
+                return 1;
         }
 
         if (args[0] == "graph") {
             Console.WriteLine(MakeGraph(tlNodes, args, file).ToText());
-            return;
+            return 0;
         }
 
-        if (args[0] == "silent") return;
+        if (args[0] == "silent") return 0;
 
         if (args[0] == "print") {
             foreach (var node in tlNodes.Select(n => (n is TopLevelStatementNode stmt) ? stmt : null).Where(n => n is not null)) {
@@ -78,15 +88,16 @@ Usage : dotnet run -- [option]
                 Console.WriteLine(s[..^2]);
             }
 
-            return;
+            return 0;
         }
 
         if (args[0] == "hash") {
             Console.WriteLine(MakeGraph(tlNodes, args, file).GetHashCode());
-            return;
+            return 0;
         }
 
         Console.WriteLine("Could not understand option " + args[0]);
+        return 1;
     }
 
     static void AddGraphPrelude(Graph g, Uri file) {

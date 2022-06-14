6fb466badb4a44cbbcbf7626531b8d5fb326c7ed
blokyk <eliot.courvoisier@gmail.com>
2022-06-14 03:13:29 +0200

    Markup styles won't 'erase' each other anymore
    Styles (as well as colors and formats, obviously) are now managed
    using a stack during rendering, meaning that when you stop using a
    style, it will restore the one that was previously used. For example,
    printing color text after having .PushStyle'ed will only affect the
    color of *that* text, and will use the right style (including color)
    for all the content that came before a potential .PopStyle().
    
    PS: It doesn't matter if all the styling wasn't popped before calling
        render, or even if there were too many that were popped ;)

---
+++ Logging/Markup/TextFormat.cs
@@ -0,0 +1,10 @@
+[Flags]
+internal enum TextFormat {
+    None = 0,
+    Reset = 1,
+    Bold = 2 * Reset,
+    Faint = 2 * Bold,
+    Underline = 2 * Faint,
+    Italic = 2 * Underline,
+    Strikethrough = 2 * Italic,
+}

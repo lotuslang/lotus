0e2a5d50d979c690ef28a9574d462951eee4e24f
blokyk <eliot.courvoisier@gmail.com>
2022-06-16 17:54:25 +0200

    Fixed invalid namespace error type
    An invalid namespace "name" would display the wrong error and crash
    the parser

--- Parselets/TopLevelParselets/NamespaceParselet.cs
+++ Parselets/TopLevelParselets/NamespaceParselet.cs
@@ -4,17 +4,25 @@ public sealed class NamespaceParslet : ITopLevelParslet<NamespaceNode>
     private static NamespaceParslet _instance = new();
     public static NamespaceParslet Instance => _instance;
 
-	private NamespaceParslet() : base() { }
+    private NamespaceParslet() : base() { }
 
     public NamespaceNode Parse(TopLevelParser parser, Token namespaceToken) {
         var name = parser.ExpressionParser.Consume();
 
-        if (namespaceToken is Token namespaceKeyword && namespaceKeyword == "namespace") {
-            if (Utilities.IsName(name)) {
-                return new NamespaceNode(name, namespaceKeyword);
-            }
+        if (namespaceToken is not Token namespaceKeyword || namespaceKeyword != "namespace") {
+            throw Logger.Fatal(new InvalidCallError(ErrorArea.Parser, namespaceToken.Location));
         }
 
-        throw Logger.Fatal(new InvalidCallError(ErrorArea.Parser, namespaceToken.Location));
+        var node = new NamespaceNode(name, namespaceKeyword);
+
+        if (!Utilities.IsName(name)) {
+            Logger.Error(new UnexpectedError<ValueNode>(ErrorArea.Parser) {
+                Value = name,
+                Expected = "a valid name",
+                As = "a namespace name",
+            });
+        }
+
+        return node;
     }
 }

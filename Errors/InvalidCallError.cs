b94ca1d08f67f2336b18b8e0837a1953cf50638e
blokyk <eliot.courvoisier@gmail.com>
2022-09-08 17:29:17 +0200

    fix(logging): fix InvalidCallError ignoring area param

--- Errors/InvalidCallError.cs
+++ Errors/InvalidCallError.cs
@@ -7,7 +7,7 @@ public class InvalidCallError : LotusError, ILocalized
     public InvalidCallError(ErrorArea area, [CallerMemberName] string caller = "<unknown-caller>", [CallerFilePath] string callerPath = "")
         : base(area, caller: caller, callerPath: callerPath) { }
     public InvalidCallError(ErrorArea area, LocationRange loc, [CallerMemberName] string caller = "<unknown-caller>", [CallerFilePath] string callerPath = "")
-        : base(ErrorArea.Parser, caller: caller, callerPath: callerPath)
+        : base(area, caller: caller, callerPath: callerPath)
     {
             Location = loc;
     }

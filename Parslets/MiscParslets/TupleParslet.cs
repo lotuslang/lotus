44c02c176eed0bdf2c2dcf0ff0d2ceccef237da0
blokyk <eliot.courvoisier@gmail.com>
2022-09-08 17:53:32 +0200

    chore(parsing): remove more unused locals

--- Parslets/MiscParslets/TupleParslet.cs
+++ Parslets/MiscParslets/TupleParslet.cs
@@ -79,8 +79,6 @@ public class TupleParslet<TParser, TPNode, TValue> : IParslet<TParser, Tuple<TVa
                     break;
                 }
 
-                var lastItem = items.Last();
-
                 // FIXME: this assumes the function used the main parser and/or set the
                 // parser's Current property, which is not always the case
                 if (!isValid || !parser.Current.IsValid) {

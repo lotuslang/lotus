d46fd231f2063328395191e0ecf7c16e86ca6e1a
blokyk <eliot.courvoisier@gmail.com>
2023-02-11 21:20:16 +0100

    fix(ast): OMG FullNameNodes DIDNT HAVE A VISIT METHOD

--- src/Compiler/Syntax/Nodes/ValueNodes/FullNameNode.cs
+++ src/Compiler/Syntax/Nodes/ValueNodes/FullNameNode.cs
@@ -7,4 +7,10 @@ public sealed record FullNameNode(OperatorToken DotToken, ImmutableArray<IdentTo
         : this(dotToken, left.Parts.AddRange(right.Parts)) {
         IsValid = left.IsValid && right.IsValid;
     }
+
+    [DebuggerHidden]
+    [DebuggerStepThrough]
+    [DebuggerNonUserCode]
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public override T Accept<T>(Visitors.IValueVisitor<T> visitor) => visitor.Visit(this);
 }

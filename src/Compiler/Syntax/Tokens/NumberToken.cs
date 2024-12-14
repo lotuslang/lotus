30f8fa98a1306b6c5dd373bb39a62d7f84bc392f
blokyk <eliot.courvoisier@gmail.com>
2024-12-14 22:06:10 +0100

    refact: split NumberKind from NumberToken definition (and .Syntax ns)

--- src/Compiler/Syntax/Tokens/NumberToken.cs
+++ src/Compiler/Syntax/Tokens/NumberToken.cs
@@ -1,18 +1,5 @@
 namespace Lotus.Syntax;
 
-[Flags]
-public enum NumberKind {
-    Unknown = 0,
-    Unsigned = 1 << 0,
-    Int = 1 << 1,
-    Long = 1 << 2,
-    Float = 1 << 3,
-    Double = 1 << 4,
-
-    UInt = Unsigned | Int,
-    ULong = Unsigned | Long
-}
-
 public sealed record NumberToken : Token
 {
     public new static readonly NumberToken NULL = new("", Double.NaN, LocationRange.NULL, default) { IsValid = false };

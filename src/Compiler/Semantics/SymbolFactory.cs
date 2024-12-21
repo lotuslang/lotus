514d3911c5b5b1d87c27b4b061549012fd534119
blokyk <eliot.courvoisier@gmail.com>
2024-12-21 13:41:36 +0100

    refact(sem): add CreateMissingSymbol

--- src/Compiler/Semantics/SymbolFactory.cs
+++ src/Compiler/Semantics/SymbolFactory.cs
@@ -121,4 +121,33 @@ internal sealed class SymbolFactory(SemanticUnit unit)
 
         return new ErrorTypeInfo(nameParts[^1], _unit) { ContainingSymbol = container };
     }
+
+    public ErrorSymbolInfo CreateMissingSymbol(string name)
+        => new(name, _unit);
+    public ErrorSymbolInfo CreateMissingSymbol(NameNode name)
+        => CreateMissingSymbol(name.ToFullString());
+
+    public ErrorSymbolInfo CreateMissingSymbol(NameNode name, Scope scope) {
+        if (name is IdentNode { Value: var singleName })
+            return new ErrorSymbolInfo(singleName, _unit);
+
+        var nameParts = ((FullNameNode)name).Parts;
+
+        SymbolInfo container = new ErrorSymbolInfo(nameParts[0], _unit);
+        var currScope = scope;
+
+        foreach (var part in nameParts.SkipLast(1)) { // no need to try to resolve the last part
+            var nextContainer = currScope.Get(part);
+
+            if (scope.Get(part) is NamespaceInfo ns)
+                nextContainer = ns;
+            else
+                nextContainer = new ErrorSymbolInfo(part, _unit) { ContainingSymbol = container };
+
+            container = nextContainer;
+            currScope = Scope.From((container as IScope)!);
+        }
+
+        return new ErrorSymbolInfo(nameParts[^1], _unit) { ContainingSymbol = container };
+    }
 }

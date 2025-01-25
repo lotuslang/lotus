a4bddcfea3fd5b13e3926fe3eaba5cffc898ec88
blokyk <courvoisier.zoe@gmail.com>
2025-01-25 21:09:16 +0100

    nit(sem): use primary ctor for UnionTypeInfo

--- src/Compiler/Semantics/Symbols/TypeInfo/UnionTypeInfo.cs
+++ src/Compiler/Semantics/Symbols/TypeInfo/UnionTypeInfo.cs
@@ -1,15 +1,12 @@
 namespace Lotus.Semantics;
 
-public sealed class UnionTypeInfo : TypeInfo
+public sealed class UnionTypeInfo(
+    IEnumerable<TypeInfo> types,
+    SemanticUnit unit
+) : TypeInfo(unit)
 {
     // todo: this should really be an ImmutableArray
-    public List<TypeInfo> Types { get; }
-
-    public UnionTypeInfo(IEnumerable<TypeInfo> types, SemanticUnit unit) : base(unit)
-        => Types = [..types];
-
-    public UnionTypeInfo(List<TypeInfo> types, SemanticUnit unit) : base(unit)
-        => Types = [..types];
+    public List<TypeInfo> Types { get; } = [.. types];
 
     public override T Accept<T>(ISymbolVisitor<T> visitor)
         => visitor.Visit(this);
